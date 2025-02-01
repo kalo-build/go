6ff1bb49b5ca903e73066f249db4d1fac0a39f16
bnunamak <brennan@nunamaker.de>
2025-02-01 17:57:42 +0100

    Enums store their name

--- pkg/godef/enum.go
+++ pkg/godef/enum.go
@@ -3,12 +3,14 @@ package godef
 import "github.com/kaloseia/clone"
 
 type Enum struct {
+	Name    string
 	Type    GoTypeDerived
 	Entries []EnumEntry
 }
 
 func (s Enum) DeepClone() Enum {
 	return Enum{
+		Name:    s.Name,
 		Type:    s.Type.DeepClone(),
 		Entries: clone.DeepCloneSlice(s.Entries),
 	}
